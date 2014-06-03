5b875b90c90b6cab7a99de764f037350193ed4ef
Hellsing <matrix11@gmx.net>
2014-06-03 17:50:25 +0200

    Fixed To-Target mode in SOW

--- common/SOW.lua
+++ common/SOW.lua
@@ -1,4 +1,4 @@
-local version = "1.129"
+local version = "1.130"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/Hellsing/BoL/master/common/SOW.lua".."?rand="..math.random(1,10000)
@@ -270,12 +270,15 @@ function SOW:OrbWalk(target, point)
 		self:Attack(target)
 	elseif self:CanMove() and self.Move then
 		if not point then
-			local OBTarget = GetTarget()
+			local OBTarget = GetTarget() or target
 			if self.Menu.Mode == 1 or not OBTarget then
 				local Mv = Vector(myHero) + 400 * (Vector(mousePos) - Vector(myHero)):normalized()
 				self:MoveTo(Mv.x, Mv.z)
-			elseif GetDistanceSqr(OBTarget) > 50*50 then
+			elseif GetDistanceSqr(OBTarget) > 100*100 + math.pow(self.VP:GetHitBox(OBTarget), 2) then
 				local point = self.VP:GetPredictedPos(OBTarget, 0, 2*myHero.ms, myHero, false)
+				if GetDistanceSqr(point) < 100*100 + math.pow(self.VP:GetHitBox(OBTarget), 2) then
+					point = Vector(Vector(myHero) - point):normalized() * 50
+				end
 				self:MoveTo(point.x, point.z)
 			end
 		else
